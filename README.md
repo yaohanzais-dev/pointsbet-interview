cccc484cb4af1e3aec92fe08cfa8126f86502b56
Yaohan <yaohanzais@gmail.com>
2026-03-17 14:19:59 +1100

    Initial commit - add readme and string formatter

---
+++ README.md
@@ -0,0 +1,10 @@
+# PointsBet_Backend_Online_Code_Test
+This is a simple folder containing a `StringFormatter.cs` file for code enhancement.
+
+For candidates, please follow the below points to complete the test:
+- Read the `README.md` file in the root directory, which should be the same file you are reading now. :)
+- Go to the `README.md` file
+  - Read the comment in the `StringFormatter.cs` file
+  - Follow the instructions in the comment to complete the task
+
+Thank you for completing the code test with PointsBet.
---
+++ StringFormatter.cs
@@ -0,0 +1,34 @@
+using System.Text;
+
+namespace PointsBet_Backend_Online_Code_Test
+{
+
+    /*
+    Improve a block of code as you see fit in C#.
+    You may make any improvements you see fit, for example:
+      - Cleaning up code
+      - Removing redundancy
+      - Refactoring / simplifying
+      - Fixing typos
+      - Any other light-weight optimisation
+    */
+    public class StringFormatter
+    {
+
+        //Code to improve
+        public static string ToCommaSepatatedList(string[] items, string quote)
+        {
+            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
+
+            if (items.Length > 1)
+            {
+                for (int i = 1; i < items.Length; i++)
+                {
+                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
+                }
+            }
+
+            return qry.ToString();
+        }
+    }
+}
